dff81b3259c2ce0ac050ca9a1e0bca9db95ca05c
trymycode <imitasingha1994@gmail.com>
2018-03-26 12:36:31 +0530

    1st commit

---
+++ script.js
@@ -0,0 +1,32 @@
+//check of specific todos by clicking
+$( " ul").on("click" ,"li" , function(){
+	$(this).toggleClass("completed" );
+});
+
+//click on X to  delete todo
+$("ul").on("click" ,"span" ,function()
+	{
+	$(this).parent().fadeOut(500,function(){
+		$(this).remove();
+	});
+
+	 event.stopPropagation();
+
+});
+ 
+
+ $("input[type=text]").keypress( function(event){
+ 	if(event.which===13){
+ 		//grab the new todo text from the input
+ 		 var todoText = $(this).val();
+ 		 //create a new li and add to ul
+
+ 		 $("ul").append("<li> <span><i class='fa fa-trash'></i></span>" +todoText+ "</li>");
+ 	}
+ });
+
+$(".fa-plus").click(function(){
+	$("input[type='text']").fadeToggle();
+});
+
+
